f0e14ae92cb7594e19035e2c1768c6a6156e097f
Rui Campos <ruinate@gmail.com>
2025-05-30 19:51:07 -0700

    added a coming soon section for the minis

--- scripts/json_test.py
+++ scripts/json_test.py
@@ -1,13 +1,13 @@
 import json
 
-with open('../projects/section_map.json') as f:
+with open('projects/section_map.json') as f:
     section_map = json.load(f)
 
 # Accessing values:
 print(section_map["cosplay"])
-print(section_map["cosplay"]["heading"])
-print(section_map["cosplay"]["descriptor"])
-print(section_map["miniatures"]["descriptor"])
+print(section_map["cosplay"]["header"])
+print(section_map["cosplay"]["description"])
+print(section_map["miniatures"]["description"])
 try:
     print(section_map["sadfsadsF"])
 except KeyError as e:
